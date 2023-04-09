47abf4312b5a01c7069ade264043aeaab83cbd40
rbase0218 <rbase@naver.com>
2023-04-09 20:09:17 +0900

    Feat : Scene Loader 추가 및 UniTask 추가

--- Assets/Script/Common/Loading/SceneLoader.cs
+++ Assets/Script/Common/Loading/SceneLoader.cs
@@ -1,8 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Assertions.Must;
 
-public class SceneLoader : MonoBehaviour
+public class SceneLoader : MonoBehaviour, IProgress<float>
 {
-    
+    private void Awake()
+    {
+        LoadScene(1);
+    }
+
+    public void LoadScene(int id)
+    {
+        gameObject.SetActive(false);
+        LoadAsyncScene(id).Forget();
+    }
+
+    public void Report(float value)
+    {
+        // value를 통해서 progress data를 가져옴
+    }
+
+    private async UniTaskVoid LoadAsyncScene(int id)
+    {
+        await SceneManager.LoadSceneAsync(id, LoadSceneMode.Single).ToUniTask(progress: this);
+    }
 }
