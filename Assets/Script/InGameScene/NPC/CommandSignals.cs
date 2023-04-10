0a7f613a9f493a7165d530404763606d529920f0
rbase0218 <rbase@naver.com>
2023-04-11 01:07:47 +0900

    Feat : NPC Controller, 등 기본 세팅

---
+++ Assets/Script/InGameScene/NPC/CommandSignals.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace OneBunny
+{
+    public class CommandSignals : MonoBehaviour, IInteractionCommand
+    {
+        
+        
+        // Open - IInteractionCommand
+        public void Open()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Script/InGameScene/NPC/NPCController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    // NPC Controller
+    
+}
